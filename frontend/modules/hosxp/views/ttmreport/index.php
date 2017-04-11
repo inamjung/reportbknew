bb3c2f9ba6744d1134c1c4d85160e54725ce98cc
swl <swl@inam>
2017-04-11 18:01:10 +0700

    11/04/2560 เพิ่มของ ศรีวิไล

--- frontend/modules/hosxp/views/ttmreport/index.php
+++ frontend/modules/hosxp/views/ttmreport/index.php
@@ -1,48 +1,31 @@
 <div class="row">
     <div class="col-xs-6 col-sm-6 col-md-6">
         <div class="panel panel-danger">
-  <div class="panel-heading"><h4>รายงาน แผนไทย </h4><h5>โดย นายจักรพงษ์ สุวรรณไตร&nbsp;&nbsp; ตำแหน่ง จพง.คอมพิวเตอร์  &nbsp;&nbsp;รพ.บึงกาฬ</h5></div>
-  <div class="panel-body">
-        <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">               
+            <div class="panel-heading"><h4>รายงาน แผนไทย </h4><h5>โดย นายจักรพงษ์ สุวรรณไตร&nbsp;&nbsp; ตำแหน่ง จพง.คอมพิวเตอร์  &nbsp;&nbsp;รพ.บึงกาฬ</h5></div>
+            <div class="panel-body">
+
                 <p>
                     <?php
-                        echo \yii\helpers\Html::a('<i class="fa fa-circle text-blue"></i> 1) จำนวนผู้ป่วย แผนกแผนไทย',
-                                ['/hosxp/ttmreport/ttmreport']);
+                    echo \yii\helpers\Html::a('<i class="fa fa-circle text-red"></i> 1) จำนวนผู้ป่วย แผนกแผนไทย', ['/hosxp/ttmreport/ttmreport']);
                     ?>
                 </p>    
-                
-               
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-6">  
-             
             </div>
         </div>
-  </div>
-</div>
     </div>
     <div class="col-xs-6 col-sm-6 col-md-6">
         <div class="panel panel-danger">
-  <div class="panel-heading"><h4>รายงาน หมวดยา </h4><h5>โดย นายชาญณรงค์ นามกว้าง&nbsp;&nbsp; ตำแหน่ง นวก.คอมพิวเตอร์  &nbsp;&nbsp;รพ.บึงกาฬ</h5></div>
-  <div class="panel-body">
-        <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">               
-                <p>
-                    <?php
-                        echo \yii\helpers\Html::a('<i class="fa fa-circle text-blue"></i> 1) สรุปยอดมูลค่าการใช้ยา IPD',
-                                ['/hosxp/phar/report03']);
-                    ?>
-                </p>    
-                
-               
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-6">  
-             
+            <div class="panel-heading"><h4>รายงาน หมวดยา </h4><h5>โดย นายชาญณรงค์ นามกว้าง&nbsp;&nbsp; ตำแหน่ง นวก.คอมพิวเตอร์  &nbsp;&nbsp;รพ.บึงกาฬ</h5></div>
+            <div class="panel-body"> 
+                    <p>
+                        <?php
+                        echo \yii\helpers\Html::a('<i class="fa fa-circle text-red"></i> 1) สรุปยอดมูลค่าการใช้ยา IPD', ['/hosxp/phar/report03']);
+                        ?>
+                    </p>               
             </div>
         </div>
-  </div>
-</div>
     </div>
 </div>
+
+
 
 
